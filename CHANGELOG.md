3f0c46cb5a0ab0435460716994ff05cd7acf804e
Robert A. Petit III <robbie.petit@gmail.com>
2023-09-07 00:01:02 +0000

    convert Path to string for prefix

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## Unreleased
 
+## 1.0.8
+
+- Fixed `bactopia-prepare` usage of `--prefix` not working
+
 ## 1.0.7
 
 - Fixed `bactopia-search` not including header name in accessions.txt
--- bactopia/cli/prepare.py
+++ bactopia/cli/prepare.py
@@ -124,7 +124,7 @@ def search_path(path, pattern, recursive=False):
 def get_path(fastq, abspath, prefix):
     fastq_path = str(fastq.absolute())
     if prefix:
-        return fastq_path.replace(abspath, prefix).replace("///", "//")
+        return fastq_path.replace(str(abspath), prefix).replace("///", "//")
     return fastq_path
 
 
